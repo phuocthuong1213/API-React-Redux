978dc5f63fca2b35b47e990fb4f38dbddf21e801
Nguyễn Phước Thượng <phuocthuong1213@gmail.com>
2020-07-24 09:35:49 +0700

    Update

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -15,17 +15,21 @@ const products = (state = initialState, action) => {
     var index = -1;
     var { id, product } = action;
     switch (action.type) {
+
         case Types.FETCH_PRODUCT:
             state = action.products;
             return [...state];
+
         case Types.DELETE_PRODUCT:
             index = findIndex(state, id);
             state.splice(index, 1);
             return [...state];
         case Types.ADD_PRODUCT:
+
             state.push(action.product);
             return [...state];
         case Types.UPDATE_PRODUCT:
+            
             index = findIndex(state, product.id);
             state[index] = product;
             return [...state];
